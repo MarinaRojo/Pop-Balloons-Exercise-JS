fa3403d1452cf5af140ee92834f6f1872802ad3b
Marina Rojo <marinarojo.cc@gmail.com>
2022-01-03 15:54:14 +0000

    Feature: Pop balloons with Javascript Exercise

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
-
+let ballonsMap = ['green','blue','red','yellow','purple','green','blue','red','yellow','purple','green','blue','red','yellow','purple','green'];
+let  activeBalloons=ballonsMap.length;
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
-    // set the color to null on the balloon position
+    ballonsMap[position] = null;
+    activeBalloons=activeBalloons-1;
     render();
 }
 
@@ -12,10 +13,13 @@ const render = () => {
     
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        let popped = (color === null) ? "popped" : "active";        
+        return `<div class='balloon ${popped}' style='background-color: ${color}; color: ${color};' onclick='popBalloon("${position}")'></div>`;
+
+        
     });
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
+    document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
 
     if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
